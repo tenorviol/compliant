e5fa2cb883879dc5c58fe35ed85da6839ebddcdd
Josh Campoverde <campo@eero.com>
2017-01-11 13:13:19 -0800

    more whitespace

--- src/main/scala/SemanticVersion.scala
+++ src/main/scala/SemanticVersion.scala
@@ -14,6 +14,7 @@ class SemanticVersion(val versionString: String) {
     this.trimmedVersionString
   }
 
+
   def majorVersion : String = {
     this.major
   }
@@ -23,19 +24,23 @@ class SemanticVersion(val versionString: String) {
     this.minor
   }
 
+
   def patchVersion : String = {
     this.patch
   }
+
 
   // slightly less straightforward attributes
   def versionStringWithoutBuildMetadata : String = {
     this.trimmedVersionString.split("\\+").head
   }
 
+
   def prerelease : String = {
     val splitByDashes = this.trimmedVersionString.split("\\-")
     splitByDashes.slice(1, splitByDashes.length).mkString("-").split("\\+").head
   }
+
 
   //
   def buildMetadata : String = {
@@ -44,15 +49,18 @@ class SemanticVersion(val versionString: String) {
     splitByPlus.slice(1,splitByPlus.length).mkString("+")
   }
 
+
   // comparators
   def ==(otherVersionString: String) : Boolean = {
     val otherVersion = new SemanticVersion(otherVersionString)
     this == otherVersion
   }
 
+
   def ==(otherVersion: SemanticVersion) : Boolean = {
     this.versionStringWithoutBuildMetadata == otherVersion.versionStringWithoutBuildMetadata
   }
+
 
   //
   def >=(otherVersion: SemanticVersion) : Boolean = {
@@ -61,45 +69,57 @@ class SemanticVersion(val versionString: String) {
     greaterThan || equalTo
   }
 
+
   // The heart of comparisons is based off of this
   // Suggestions for improvement welcome
   def >(otherVersion: SemanticVersion) : Boolean = {
     if (this.majorVersion > otherVersion.majorVersion) {
       true
     } else if (this.majorVersion == otherVersion.majorVersion) {
+
       if (this.minorVersion > otherVersion.minorVersion) {
         true
       } else if (this.minorVersion == otherVersion.minorVersion) {
+
         if (this.patchVersion > otherVersion.patchVersion) {
           true
         } else if (this.patchVersion == otherVersion.patchVersion && this.prerelease > otherVersion.prerelease) {
+
           true
         } else {
+
           false // major, minor, patch version are same, prerelease is less
         }
       } else {
+
         false // major version is same, and minor version is less
       }
+
     } else {
       false // major version is less
     }
   }
+
 
   //
   def <=(otherVersion: SemanticVersion) : Boolean = {
     if (this == otherVersion) {
       true
     } else if (this > otherVersion) {
+
       false
     } else {
+
       true
     }
   }
 
   //
   def <(otherVersion: SemanticVersion) : Boolean = {
+
     val notGreaterThan = !(this > otherVersion)
     val notEqualTo = !(this == otherVersion)
+
     notGreaterThan && notEqualTo
   }
 
