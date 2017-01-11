2d906e5f303fea0c8f991cb9b78ac6ba25e6b2cb
Josh Campoverde <campo@eero.com>
2017-01-10 16:11:16 -0800

    Very beginning

---
+++ README.md
@@ -0,0 +1,6 @@
+A new Semantic Versioning library.
+
+The goals are:
+- Implement the SemVer spec as closely as possible
+- Handle the precedence of pre-release builds accurately
+- Handle comparisions that include build metadata correctly
---
+++ SemanticVersion.scala
@@ -0,0 +1,71 @@
+class SemanticVersion(val versionString: String) {
+  val trimmedVersionString = versionString.trim.replaceAll("\\s", "")
+  // val major = "ma" // reg ex: ^([^.]*).*
+  val splitByPeriods = trimmedVersionString.split("\\.")
+  // val major = "^([^.]*)".r.findFirstIn(trimmedVersionString).getOrElse("")
+  val major = splitByPeriods(0)
+  val minor = splitByPeriods(1)
+  val patch = splitByPeriods.slice(2,splitByPeriods.length).mkString(".").split("\\+|\\-")(0)
+
+  def fullVersionString: String = {
+    this.trimmedVersionString
+  }
+
+  def majorVersion : String = {
+    this.major
+  }
+
+  def minorVersion : String = {
+    this.minor
+  }
+
+  def patchVersion : String = {
+    this.patch
+  }
+
+  def versionStringWithoutBuildMetadata : String = {
+    this.trimmedVersionString.split("\\+").head
+  }
+
+  def prerelease : String = {
+    // TODO: Implement this
+    "sample-prerelease"
+  }
+
+  def buildMetadata : String = {
+    val splitByPlus = this.trimmedVersionString.split("\\+")
+    // join with plus to recreate original string
+    splitByPlus.slice(1,splitByPlus.length).mkString("+")
+  }
+
+  def ==(otherVersionString: String) : Boolean = {
+    val otherVersion = new SemanticVersion(otherVersionString)
+    this == otherVersion
+  }
+
+  def ==(otherVersion: SemanticVersion) : Boolean = {
+    // TODO: Implement this
+    // true
+    this.versionStringWithoutBuildMetadata == otherVersion.versionStringWithoutBuildMetadata
+  }
+
+  def >=(otherVersion: SemanticVersion) : Boolean = {
+    // TODO: Implement this
+    true
+  }
+
+  def >(otherVersion: SemanticVersion) : Boolean = {
+    // TODO: Implement this
+    true
+  }
+
+  def <=(otherVersion: SemanticVersion) : Boolean = {
+    // TODO: Implement this
+    true
+  }
+
+  def <(otherVersion: SemanticVersion) : Boolean = {
+    // TODO: Implement this
+    true
+  }
+}
